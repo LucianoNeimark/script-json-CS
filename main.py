704eece9ebd410a8375e493925283c3a83f1b942
Luciano <38950566+LucianoNeimark@users.noreply.github.com>
2022-10-04 23:07:18 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+
+import csv
+import json
+
+
+def make_json(csvFilePath, jsonFilePath):
+    data = []
+
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+
+        for rows in csvReader:
+            rowDict = {
+                "name": rows['Nombre completo'],
+                "title": rows['Departamento'],
+                "social": [
+                    {
+                        "logo": "instagram",
+                        "href": rows['Instagram'],
+                    },
+                    {
+                        "logo": "linkedin",
+                        "href": rows['Linkedin'],
+                    },
+                    {
+                        "logo": "github",
+                        "href": rows['Github'],
+                    },
+
+                ],
+                "image": {
+                    "src": "",
+                    "alt": rows['Nombre completo']
+                }
+            }
+            data.append(rowDict.copy())
+
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+
+
+csvFilePath = r'/Users/lucho/Desktop/Equipo.csv'
+jsonFilePath = r'/Users/lucho/Desktop/out.json'
+
+make_json(csvFilePath, jsonFilePath)
