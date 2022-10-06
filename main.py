380f49af77d9d6e8bdbc1b37796e62b836b1a2fe
srosati <srosati@itba.edu.ar>
2022-10-06 11:30:23 -0300

    Added Picture + Skip invalid rows

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-from audioop import add
 import csv
 import json
 import sys
 import os.path
-import firebase_admin
-from firebase_admin import firestore
-from firebase_admin import credentials
+from firebase_admin import firestore, credentials, initialize_app
 
 
 def main():
-
     if len(sys.argv) <= 2 or len(sys.argv) > 4:
-        print("Usage: ./main.py inFile.csv key.json [outFile.json]")
+        print("Usage: ./main.py inFile.csv key.json")
         return -1
 
-    outFile = './out.json' if len(sys.argv) == 3 else sys.argv[3]
     inFile = sys.argv[1]
     key = sys.argv[2]
 
@@ -23,51 +18,58 @@ def main():
         return -1
 
     if not os.path.isfile(key) or not key.endswith(".json"):
-        print("Invalid csv input file")
+        print("Invalid key file")
         return -1
 
     cred = credentials.Certificate(key)
-    app = firebase_admin.initialize_app(cred)
-    db = firestore.client()
+    initialize_app(cred)
+    
+    data, ids = make_json(inFile)
+    addTeam(ids, data)
 
-    data, ids = make_json(inFile, outFile)
-    addToCollection(data, ids, db)
 
-
-def make_json(csvFilePath, jsonFilePath):
+def make_json(csvFilePath):
     data = []
     ids = []
     with open(csvFilePath, encoding='utf-8') as csvf:
         csvReader = csv.DictReader(csvf)
-        for rows in csvReader:
+        for row in csvReader:
+            if invalidRow(row):
+                print('Invalid row, skipping')
+                print(row)
+                continue
+            
             rowDict = {
-                "name": rows['Nombre completo'],
-                "title": rows['Departamento'],
+                "name": row['Nombre completo'],
+                "title": row['Departamento'],
                 "social": [],
                 "image": {
-                    "src": "",
-                    "alt": rows['Nombre completo']
+                    "src": row['Foto'],
+                    "alt": row['Nombre completo']
                 }
             }
-            socialMediaSources = ["Instagram", "Linkedin", "Github"]
+
+            socialMediaSources = ["Instagram", "Linkedin", "Github", "WebPersonal"]
             for source in socialMediaSources:
-                sourceVal = rows[source]
+                sourceVal = row[source]
                 if sourceVal != "":
                     rowDict["social"].append({
                         "logo": source.lower(),
                         "href": sourceVal,
                     })
             data.append(rowDict.copy())
-            ids.append(rows["Legajo"])
-    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
-        jsonf.write(json.dumps(data, indent=2))
+            ids.append(row['Legajo'])
 
     return data, ids
 
 
-def addToCollection(team, ids, db):
-    for i, member in enumerate(team):
-        db.collection(u'team').document(ids[i]).set(member)
+def addTeam(ids, team):
+    db = firestore.client()
+    for idx, member in enumerate(team):
+        db.collection(u'team').document(ids[idx]).set(member)
+
+def invalidRow(row):
+    return row['Legajo'] == '' or row['Foto'] == '' or row['Nombre completo'] == '' or row['Departamento'] == ''
 
 
 if __name__ == "__main__":
